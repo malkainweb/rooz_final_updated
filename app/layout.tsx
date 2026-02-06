198cc4d2bdeb57d3f27feb9bff6782c8bd738d47
Davis Isibor <davisisibor@gmail.com>
2026-02-06 04:40:32 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,18 +108,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script
-        id="Bing clarity"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", 'vcek3czin5');`,
-        }}
-      />
       <head>
         {/* Additional SEO tags */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
@@ -133,7 +121,18 @@ export default function RootLayout({
         <link rel="preconnect" href="https://cal.com" />
         <link rel="dns-prefetch" href="https://cal.com" />
       </head>
-
+      <Script
+        id="Bing clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", 'vcek3czin5');`,
+        }}
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
